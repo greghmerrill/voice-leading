281bce151c515a8fef5180cb9fb56648cc1d2709
greghmerrill <greghmerrill@gmail.com>
2014-01-09 20:59:49 -0600

    cleanup, unit test, and add voice crossing check

--- voice-leading-validator.js
+++ voice-leading-validator.js
@@ -1,10 +1,82 @@
 // Copyright (c) 2014  Greg Merrill
 // See https://github.com/greghmerrill/voice-leading for license
 
+var NOTE_OFFSETS = { 'C' : 0, 'D' : 2, 'E' : 4, 'F' : 5, 'G' : 7, 'A' : 9, 'B' : 11 };
+var VOCAL_RANGE = $.map(['E2-C4', 'C3-F4', 'G3-D5', 'C4-A7'], function(pair) {
+  return { low: newNote(pair[0], +pair[1], 0, 0).magnitude(), high: newNote(pair[3], +pair[4], 0, 0).magnitude() };
+});
+var SPACING = { 0: 19, 1: 12, 2: 12, 3: 12 };
+var VOICE_NAME = { 0: 'Bass', 1: 'Tenor', 2: 'Alto', 3: 'Soprano' };
+
+var RULES = {
+  "Vocal Range" : function(chords, violations) {
+    $.each(chords, function(p, chord) {
+      $.each(chord.notes, function(i, note) {
+        var range = VOCAL_RANGE[i];
+        if (range.low > note.magnitude()) {
+          violations.push({ error: "Too low for vocal range: measure " + chord.measure + " " + note});
+        }
+        else if (range.high < note.magnitude()) {
+          violations.push({ error: "Too high for vocal range: measure " + chord.measure + " " + note});
+        }
+      });
+    });
+  },
+  "Spacing Between Voices" : function(chords, violations) {
+    $.each(chords, function(p, chord) {
+      if (chord.notes.length == 4) {
+        for (var i = 0; i < 3; i++) {
+          var spacing = SPACING[i];
+          var n1 = chord.notes[i];
+          var n2 = chord.notes[i + 1];
+          if (n2.magnitude() - n1.magnitude() > spacing) {
+            violations.push({ error: "Excessive spacing between " + VOICE_NAME[i] + " and " + VOICE_NAME[i + 1] + ": measure " + chord.measure + " " + n1 + " > " + n2});
+          }
+        }
+      }
+    });
+  },
+  "Parallel Fifths and Octaves" : function(chords, violations) {
+    var prevChord;
+    var prevChordIntervals;
+    $.each(chords, function(p, chord) {
+      var chordIntervals = chord.getIntervals();
+      if (prevChordIntervals) {
+        // Compare current chord with prev to find parallel 5ths, 8ves
+        $.each(chordIntervals, function(i, interval) {
+          $.each(prevChordIntervals, function(j, prevInterval) {
+            if (prevInterval.movesParallelTo(interval)) {
+              if (interval.delta == 7) {
+                violations.push({ error: "Parallel Fifths: measure " + prevChord.measure + ' ' + prevInterval + ' to measure ' + chord.measure + ' ' + interval});
+              }
+              else if (interval.delta == 12) {
+                violations.push({ error: "Parallel Octaves: measure " + prevChord.measure + ' ' + prevInterval + ' to measure ' + chord.measure + ' ' + interval});
+              }
+            }
+          });
+        });
+      }
+      prevChord = chord;
+      prevChordIntervals = chordIntervals;
+    });
+  },
+  "Voice Crossing" : function(chords, violations) {
+    $.each(chords, function(p, chord) {
+      if (chord.notes.length == 4) {
+        for (var i = 0; i < 3; i++) {
+          var n1 = chord.notes[i];
+          var n2 = chord.notes[i + 1];
+          if (n2.magnitude() < n1.magnitude()) {
+            violations.push({ error: "Voices crossed: " + VOICE_NAME[i] + " and " + VOICE_NAME[i + 1] + ": measure " + chord.measure + " " + n1 + " > " + n2});
+          }
+        }
+      }
+    });
+  }
+}
+
 function validate(xml) {
   var measures = parseMeasures(xml);
-
-  var violations = [];
 
   var consolidatedChords = [];
   $.each(measures, function(i, m) { 
@@ -13,79 +85,42 @@ function validate(xml) {
       consolidatedChords.push(chord);
     });
   });
-  
-  var prevChord;
-  var prevChordIntervals;
-  $.each(consolidatedChords, function(p, chord) {
-    $.each(chord.notes, function(i, note) {
-      var range = VOCAL_RANGE[i];
-      if (range.low > note.magnitude()) {
-        violations.push({ error: "Too low for vocal range: measure " + chord.measure + " " + note});
-      }
-      else if (range.high < note.magnitude()) {
-        violations.push({ error: "Too high for vocal range: measure " + chord.measure + " " + note});
-      }
-    });
-    
-    if (chord.notes.length == 4) {
-      for (var i = 0; i < 3; i++) {
-        var spacing = SPACING[i];
-        var n1 = chord.notes[i];
-        var n2 = chord.notes[i + 1];
-        if (n2.magnitude() - n1.magnitude() > spacing) {
-          violations.push({ error: "Excessive spacing between " + VOICE_NAME[i] + " and " + VOICE_NAME[i + 1] + ": measure " + chord.measure + " " + n1 + " > " + n2});
-        }
-      }
-    }
-    
-    var chordIntervals = chord.getIntervals();
-    if (prevChordIntervals) {
-      // Compare current chord with prev to find parallel 5ths, 8ves
-      $.each(chordIntervals, function(i, interval) {
-        $.each(prevChordIntervals, function(j, prevInterval) {
-          if (prevInterval.movesParallelTo(interval)) {
-            if (interval.delta == 7) {
-              violations.push({ error: "Parallel 5ths: measure " + prevChord.measure + ' ' + prevInterval + ' to measure ' + chord.measure + ' ' + interval});
-            }
-            else if (interval.delta == 12) {
-              violations.push({ error: "Parallel Octaves: measure " + prevChord.measure + ' ' + prevInterval + ' to measure ' + chord.measure + ' ' + interval});
-            }
-          }
-        });
-      });
-    }
-    prevChord = chord;
-    prevChordIntervals = chordIntervals;
-  });
 
+  var violations = [];
+  $.each(["Vocal Range", "Spacing Between Voices", "Parallel Fifths and Octaves", "Voice Crossing"], function(i, rule) {
+    RULES[rule].call(this, consolidatedChords, violations);
+  });
+  
   return violations;
 }
 
 function parseMeasures(xml) {
+  var clefByStaff = {};
+  $(xml).find('clef').each(function() {
+    var sign = $($(this).find('sign')[0]).text();
+    clefByStaff[+$(this).attr('number')] = sign;
+  });
+
   var measures = [];
   $(xml).find('part').each(function() {
     $(this).find('measure').each(function() {
       var i = $(this).attr('number') - 1;
       var measure = measures[i] || { number: i + 1, chords: {} };
       measures[i] = measure;
-      parseMeasure($(this), measure);
+      parseMeasure($(this), measure, clefByStaff);
     });
   });
   
   $(measures).each(function(i, m) { 
     $.each(m.chords, function(p, chord) {
-      chord.notes.sort(function(a, b) { return a.magnitude() - b.magnitude(); });
+      chord.notes.sort(function(a, b) { 
+        var delta = a.clef.charCodeAt() != b.clef.charCodeAt(); // F < G :-)
+        return delta != 0 ? delta : a.magnitude() - b.magnitude(); 
+      });
     });
   });
   return measures;
 }
-
-var NOTE_OFFSETS = { 'C' : 0, 'D' : 2, 'E' : 4, 'F' : 5, 'G' : 7, 'A' : 9, 'B' : 11 };
-var VOCAL_RANGE = $.map(['E2-C4', 'C3-F4', 'G3-D5', 'C4-A7'], function(pair) {
-  return { low: newNote(pair[0], +pair[1], 0, 0).magnitude(), high: newNote(pair[3], +pair[4], 0, 0).magnitude() };
-});
-var SPACING = { 0: 19, 1: 12, 2: 12, 3: 12 };
-var VOICE_NAME = { 0: 'Bass', 1: 'Tenor', 2: 'Alto', 3: 'Soprano' };
 
 function newInterval(lowNote, highNote, lowVoice, highVoice) {
   return {
@@ -118,20 +153,22 @@ function newChord(pos) {
   };
 }
 
-function newNoteFromXml(node) {
+function newNoteFromXml(node, clefByStaff) {
   return newNote(
     node.find('pitch').find('step').text(),
     node.find('pitch').find('octave').text(),
     +node.find('pitch').find('alter').text(),
-    +node.find('duration').text());
+    +node.find('duration').text(),
+    clefByStaff[+node.find('staff').text()]);
 }
 
-function newNote(step, octave, alter, duration) {
+function newNote(step, octave, alter, duration, clef) {
   return {
     step: step,
     octave: octave,
     alter: alter,
     duration: duration,
+    clef: clef,
     getSymbol: function() { 
       return this.step + ({ '-1': 'b', 'bb': '--', '1': '#', '2': '##' }[this.alter] || '') + this.octave;
     },
@@ -140,7 +177,7 @@ function newNote(step, octave, alter, duration) {
   };
 }
 
-function parseMeasure(xml, measure) {
+function parseMeasure(xml, measure, clefByStaff) {
   var pos = 0;
   var prevNote;
   $(xml).find('note, backup').each(function() {
@@ -163,10 +200,9 @@ function parseMeasure(xml, measure) {
         var chord = measure.chords[pos] || newChord(pos);
         measure.chords[pos] = chord;
 
-        var note = newNoteFromXml(node);
+        var note = newNoteFromXml(node, clefByStaff);
         
         chord.notes.push(note);
-        
         prevNote = note;
       }
       pos += duration;
